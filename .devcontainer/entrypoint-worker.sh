faf6befbefb9da9dbaaf2a5201d870af9ef5011c
Eric <ulmaskuloverik@gmail.com>
2026-07-27 12:02:56 +0300

    fix(...): some fixes

--- .devcontainer/entrypoint-worker.sh
+++ .devcontainer/entrypoint-worker.sh
@@ -5,7 +5,7 @@ set -e
 until nc -z spark-master 9000; do sleep 2; done
 
 # HDFS DataNode
- $HADOOP_HOME/sbin/hdfs-daemon.sh start datanode
+ $HADOOP_HOME/bin/hdfs --daemon start datanode
 
 # Spark Worker
  $SPARK_HOME/sbin/start-worker.sh spark://spark-master:7077
